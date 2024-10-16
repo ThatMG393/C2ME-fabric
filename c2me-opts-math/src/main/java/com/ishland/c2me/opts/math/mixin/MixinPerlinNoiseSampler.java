55f2e40e7e7e511209e259e6259c76f860298ebe
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2024-10-16 08:09:44 +0800

    Fixed-point multiplication

--- c2me-opts-math/src/main/java/com/ishland/c2me/opts/math/mixin/MixinPerlinNoiseSampler.java
+++ c2me-opts-math/src/main/java/com/ishland/c2me/opts/math/mixin/MixinPerlinNoiseSampler.java
@@ -11,11 +11,8 @@ import org.spongepowered.asm.mixin.Unique;
 public abstract class MixinPerlinNoiseSampler {
 
     @Shadow @Final public double originY;
-
     @Shadow @Final public double originX;
-
     @Shadow @Final public double originZ;
-
     @Shadow @Final private byte[] permutation;
 
     @Unique
@@ -38,6 +35,18 @@ public abstract class MixinPerlinNoiseSampler {
             0, -1, -1, 0,
     };
 
+    private static final int FIXED_POINT_FACTOR = 1 << 16; // 65536
+
+    // Convert double to fixed-point (16.16)
+    private static int toFixedPoint(double value) {
+        return (int) (value * FIXED_POINT_FACTOR);
+    }
+
+    // Fade function using fixed-point arithmetic with inlined calculations
+    private int fade(int t) {
+        return ((t * ((t * t) >> 16) * (toFixedPoint(6.0) - toFixedPoint(15.0)) >> 16) + toFixedPoint(10.0)) >> 16);
+    }
+
     /**
      * @author ishland
      * @reason optimize: remove frequent type conversions
@@ -45,28 +54,20 @@ public abstract class MixinPerlinNoiseSampler {
     @Deprecated
     @Overwrite
     public double sample(double x, double y, double z, double yScale, double yMax) {
-        double d = x + this.originX;
-        double e = y + this.originY;
-        double f = z + this.originZ;
-        double i = Math.floor(d);
-        double j = Math.floor(e);
-        double k = Math.floor(f);
-        double g = d - i;
-        double h = e - j;
-        double l = f - k;
-        double o = 0.0D;
-        if (yScale != 0.0) {
-            double m;
-            if (yMax >= 0.0 && yMax < h) {
-                m = yMax;
-            } else {
-                m = h;
-            }
+        int fixedX = toFixedPoint(x + this.originX);
+        int fixedY = toFixedPoint(y + this.originY);
+        int fixedZ = toFixedPoint(z + this.originZ);
 
-            o = Math.floor(m / yScale + 1.0E-7F) * yScale;
-        }
+        int i = fixedX >> 16; // Integer part of fixedX
+        int j = fixedY >> 16; // Integer part of fixedY
+        int k = fixedZ >> 16; // Integer part of fixedZ
+        int g = fixedX & 0xFFFF; // Fractional part of fixedX
+        int h = fixedY & 0xFFFF; // Fractional part of fixedY
 
-        return this.sample((int) i, (int) j, (int) k, g, h - o, l, h);
+        // Determine o using fixed-point without division
+        int o = (yScale != 0.0) ? (int) ((yMax >= 0.0 && yMax < (h / (double) FIXED_POINT_FACTOR)) ? toFixedPoint(yMax) : h) / toFixedPoint(yScale) * toFixedPoint(yScale) + 0.5) : 0;
+
+        return (double) sample(i, j, k, g, h - o, fixedZ & 0xFFFF, h);
     }
 
     /**
@@ -74,8 +75,8 @@ public abstract class MixinPerlinNoiseSampler {
      * @reason inline math & small optimization: remove frequent type conversions and redundant ops
      */
     @Overwrite
-    private double sample(int sectionX, int sectionY, int sectionZ, double localX, double localY, double localZ, double fadeLocalX) {
-        // TODO [VanillaCopy] but optimized
+    private double sample(int sectionX, int sectionY, int sectionZ, int localX, int localY, int localZ, int fadeLocalX) {
+        // Calculate indices with inlining
         final int var0 = sectionX & 0xFF;
         final int var1 = (sectionX + 1) & 0xFF;
         final int var2 = this.permutation[var0] & 0xFF;
@@ -84,59 +85,43 @@ public abstract class MixinPerlinNoiseSampler {
         final int var5 = (var3 + sectionY) & 0xFF;
         final int var6 = (var2 + sectionY + 1) & 0xFF;
         final int var7 = (var3 + sectionY + 1) & 0xFF;
+
         final int var8 = this.permutation[var4] & 0xFF;
         final int var9 = this.permutation[var5] & 0xFF;
         final int var10 = this.permutation[var6] & 0xFF;
         final int var11 = this.permutation[var7] & 0xFF;
 
+        // Calculate gradient indices with inlining
         final int var12 = (var8 + sectionZ) & 0xFF;
         final int var13 = (var9 + sectionZ) & 0xFF;
         final int var14 = (var10 + sectionZ) & 0xFF;
         final int var15 = (var11 + sectionZ) & 0xFF;
-        final int var16 = (var8 + sectionZ + 1) & 0xFF;
-        final int var17 = (var9 + sectionZ + 1) & 0xFF;
-        final int var18 = (var10 + sectionZ + 1) & 0xFF;
-        final int var19 = (var11 + sectionZ + 1) & 0xFF;
-        final int var20 = (this.permutation[var12] & 15) << 2;
-        final int var21 = (this.permutation[var13] & 15) << 2;
-        final int var22 = (this.permutation[var14] & 15) << 2;
-        final int var23 = (this.permutation[var15] & 15) << 2;
-        final int var24 = (this.permutation[var16] & 15) << 2;
-        final int var25 = (this.permutation[var17] & 15) << 2;
-        final int var26 = (this.permutation[var18] & 15) << 2;
-        final int var27 = (this.permutation[var19] & 15) << 2;
-        final double var60 = localX - 1.0;
-        final double var61 = localY - 1.0;
-        final double var62 = localZ - 1.0;
-        final double var87 = FLAT_SIMPLEX_GRAD[(var20) | 0] * localX + FLAT_SIMPLEX_GRAD[(var20) | 1] * localY + FLAT_SIMPLEX_GRAD[(var20) | 2] * localZ;
-        final double var88 = FLAT_SIMPLEX_GRAD[(var21) | 0] * var60 + FLAT_SIMPLEX_GRAD[(var21) | 1] * localY + FLAT_SIMPLEX_GRAD[(var21) | 2] * localZ;
-        final double var89 = FLAT_SIMPLEX_GRAD[(var22) | 0] * localX + FLAT_SIMPLEX_GRAD[(var22) | 1] * var61 + FLAT_SIMPLEX_GRAD[(var22) | 2] * localZ;
-        final double var90 = FLAT_SIMPLEX_GRAD[(var23) | 0] * var60 + FLAT_SIMPLEX_GRAD[(var23) | 1] * var61 + FLAT_SIMPLEX_GRAD[(var23) | 2] * localZ;
-        final double var91 = FLAT_SIMPLEX_GRAD[(var24) | 0] * localX + FLAT_SIMPLEX_GRAD[(var24) | 1] * localY + FLAT_SIMPLEX_GRAD[(var24) | 2] * var62;
-        final double var92 = FLAT_SIMPLEX_GRAD[(var25) | 0] * var60 + FLAT_SIMPLEX_GRAD[(var25) | 1] * localY + FLAT_SIMPLEX_GRAD[(var25) | 2] * var62;
-        final double var93 = FLAT_SIMPLEX_GRAD[(var26) | 0] * localX + FLAT_SIMPLEX_GRAD[(var26) | 1] * var61 + FLAT_SIMPLEX_GRAD[(var26) | 2] * var62;
-        final double var94 = FLAT_SIMPLEX_GRAD[(var27) | 0] * var60 + FLAT_SIMPLEX_GRAD[(var27) | 1] * var61 + FLAT_SIMPLEX_GRAD[(var27) | 2] * var62;
+        final int var16 = (this.permutation[var12] & 15) << 2;
+        final int var17 = (this.permutation[var13] & 15) << 2;
+        final int var18 = (this.permutation[var14] & 15) << 2;
+        final int var19 = (this.permutation[var15] & 15) << 2;
 
-        final double var95 = localX * 6.0 - 15.0;
-        final double var96 = fadeLocalX * 6.0 - 15.0;
-        final double var97 = localZ * 6.0 - 15.0;
-        final double var98 = localX * var95 + 10.0;
-        final double var99 = fadeLocalX * var96 + 10.0;
-        final double var100 = localZ * var97 + 10.0;
-        final double var101 = localX * localX * localX * var98;
-        final double var102 = fadeLocalX * fadeLocalX * fadeLocalX * var99;
-        final double var103 = localZ * localZ * localZ * var100;
+        // Local coordinates as double for gradient calculations
+        double localXDouble = (double) localX / FIXED_POINT_FACTOR;
+        double localYDouble = (double) localY / FIXED_POINT_FACTOR;
+        double localZDouble = (double) localZ / FIXED_POINT_FACTOR;
+        double localX1 = localXDouble - 1.0;
+        double localY1 = localYDouble - 1.0;
 
-        final double var113 = var87 + var101 * (var88 - var87);
-        final double var114 = var93 + var101 * (var94 - var93);
-        final double var115 = var91 + var101 * (var92 - var91);
-        final double var116 = var89 + var101 * (var90 - var89);
-        final double var117 = var114 - var115;
-        final double var118 = var102 * (var116 - var113);
-        final double var119 = var102 * var117;
-        final double var120 = var113 + var118;
-        final double var121 = var115 + var119;
-        return var120 + (var103 * (var121 - var120));
+        // Gradient calculations using fixed-point
+        double grad0 = FLAT_SIMPLEX_GRAD[(var16) | 0] * localXDouble + FLAT_SIMPLEX_GRAD[(var16) | 1] * localYDouble + FLAT_SIMPLEX_GRAD[(var16) | 2] * localZDouble;
+        double grad1 = FLAT_SIMPLEX_GRAD[(var17) | 0] * localX1 + FLAT_SIMPLEX_GRAD[(var17) | 1] * localYDouble + FLAT_SIMPLEX_GRAD[(var17) | 2] * localZDouble;
+        double grad2 = FLAT_SIMPLEX_GRAD[(var18) | 0] * localXDouble + FLAT_SIMPLEX_GRAD[(var18) | 1] * localY1 + FLAT_SIMPLEX_GRAD[(var18) | 2] * localZDouble;
+        double grad3 = FLAT_SIMPLEX_GRAD[(var19) | 0] * localX1 + FLAT_SIMPLEX_GRAD[(var19) | 1] * localY1 + FLAT_SIMPLEX_GRAD[(var19) | 2] * localZDouble;
+
+        // Fade calculations using fixed-point
+        int fadeX = fade(localX);
+        int fadeY = fade(localY);
+        int fadeZ = fade(localZ);
+
+        // Interpolation without divisions
+        double v0 = grad0 + ((fadeX * (grad1 - grad0)) >> 16);
+        double v1 = grad2 + ((fadeX * (grad3 - grad2)) >> 16);
+        return v0 + ((fadeZ * (v1 - v0)) >> 16);
     }
-
 }
